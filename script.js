f7048ce059173698f8d6431a527126444278e3ba
Natalena Groom <ng0745824@otc.edu>
2018-11-17 12:34:41 -0600

    Project

---
+++ script.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.listen(3000);n
